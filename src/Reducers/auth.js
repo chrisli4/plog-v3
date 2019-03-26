ba24c456f6c75a38150aa9422dd92bcc83c0cb76
chrisli4 <chrisli41@gmail.com>
2019-03-26 11:54:25 -0700

    Update auth reducer

--- src/Reducers/auth.js
+++ src/Reducers/auth.js
@@ -1,84 +1,14 @@
 import { handleActions } from 'redux-actions';
-import {
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  LOGIN_FAILURE,
-  LOGOUT_REQUEST,
-  LOGOUT_SUCCESS,
-  LOGOUT_FAILURE,
-  SIGNUP_REQUEST,
-  SIGNUP_SUCCESS,
-  SIGNUP_FAILURE,
-  SYNC_USER,
-} from '../Constants/auth';
+import * as types from '../Constants/auth';
 
 const initialState = {
-  loggedIn: false,
-  isFetching: false,
-  hasError: false,
-  signUpSuccess: false,
-  errorMessage: '',
+  error: '',
   user: null,
 };
 
 export default handleActions(
   {
-    [LOGIN_REQUEST]: state => ({
-      ...state,
-      isFetching: true,
-    }),
-
-    [LOGIN_SUCCESS]: state => ({
-      ...state,
-      isFetching: false,
-      loggedIn: true,
-      errorMessage: '',
-    }),
-
-    [LOGIN_FAILURE]: (state, action) => ({
-      ...state,
-      isFetching: false,
-      loggedIn: false,
-      hasError: true,
-      errorMessage: action.payload.message,
-    }),
-
-    [LOGOUT_REQUEST]: state => ({
-      ...state,
-      isFetching: true,
-    }),
-
-    [LOGOUT_SUCCESS]: () => ({
-      ...initialState,
-    }),
-
-    [LOGOUT_FAILURE]: (state, action) => ({
-      ...state,
-      isFetching: false,
-      loggedIn: true,
-      hasError: true,
-      errorMessage: action.payload.message,
-    }),
-
-    [SIGNUP_REQUEST]: state => ({
-      ...state,
-      isFetching: true,
-    }),
-
-    [SIGNUP_SUCCESS]: () => ({
-      ...initialState,
-      signUpSuccess: true,
-    }),
-
-    [SIGNUP_FAILURE]: (state, action) => ({
-      ...state,
-      isFetching: false,
-      loggedIn: false,
-      hasError: true,
-      errorMessage: action.payload.message,
-    }),
-
-    [SYNC_USER]: (state, action) => ({
+    [types.SYNC_USER]: (state, action) => ({
       ...state,
       user: action.payload.user,
     }),
